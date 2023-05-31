800df34a063afaefc186e768f9271a4a5c086bbc
10018879 <10018879@sbstudents.org>
2023-05-31 10:37:51 -0400

    dsvvdsvdsvds

--- src/Asteroid.java
+++ src/Asteroid.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Asteroid {
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Asteroid extends JLabel {
+
+    private final int Width = 50;
+    private final int Height = 119;
+
+    Asteroid() throws IOException {
+        setBounds(0 ,0, Width, Height);
+
+        URL asteroidUrl = new URL("https://i.ibb.co/swtv5Gp/Asteroid.png");
+        BufferedImage asteroidImage = ImageIO.read(asteroidUrl);
+        BufferedImage a = resize(asteroidImage, Width, Height);
+        ImageIcon asteroidImageIcon = new ImageIcon(a);
+
+        setIcon(asteroidImageIcon);
+    }
+    public static BufferedImage resize(BufferedImage img, int newW, int newH) {
+        Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
+        BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D g2d = dimg.createGraphics();
+        g2d.drawImage(tmp, 0, 0, null);
+        g2d.dispose();
+
+        return dimg;
+    }
 }
--- src/BlasterGame.java
+++ src/BlasterGame.java
@@ -1,3 +1,29 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class BlasterGame {
 
+    BlasterGame() throws IOException {
+        JFrame frame = new JFrame("Asteroid Blaster Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 600);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setLayout(null);
+
+        frame.getContentPane().setBackground(Color.BLACK);
+
+        frame.add(new Asteroid());
+        ImageIcon x = new ImageIcon("src/ee.gif");
+        JLabel spaceship = new JLabel(x);
+        spaceship.setBounds(50,300, 75, 75);
+        spaceship.setOpaque(true);
+        frame.add(spaceship);
+
+        frame.setVisible(true);
+    }
 }
